1bf5f96aa560867c7226a660ba1c86016226cfc8
Buglloc <buglloc@yandex.ru>
2015-01-10 13:46:46 +0200

    Добавлена возможность проверки подключения учетной записи

--- brick/window/edit_account_window_linux.cpp
+++ brick/window/edit_account_window_linux.cpp
@@ -8,6 +8,49 @@ extern char _binary_window_edit_account_glade_size;
 namespace {
 
     static void
+    on_check_button(GtkWidget *widget, EditAccountWindow *self) {
+      Account::AuthResult auth_result = self->window_objects_.account->Auth();
+      GtkMessageType dialog_type;
+      std::string body;
+      if (auth_result.success) {
+        dialog_type = GTK_MESSAGE_INFO;
+        body = "Successful!";
+      } else {
+        dialog_type = GTK_MESSAGE_ERROR;
+
+        switch (auth_result.error_code) {
+          case Account::ERROR_CODE::HTTP:
+            body = "HTTP error: " + auth_result.http_error;
+            break;
+          case Account::ERROR_CODE::CAPTCHA:
+            body = "You have exceeded the maximum number of login attempts.\n"
+               "Please log in first <b>browser</b>";
+            break;
+          case Account::ERROR_CODE::OTP:
+            body = "Account used two-step authentication.\n"
+               "Please use the <b>Application Password</b> for authorization";
+            break;
+          default:
+            body = "An unknown error occurred :(";
+            break;
+        }
+      }
+
+      GtkWidget *dialog = gtk_message_dialog_new_with_markup(
+         GTK_WINDOW(self->window_objects_.window),
+         GTK_DIALOG_DESTROY_WITH_PARENT,
+         dialog_type,
+         GTK_BUTTONS_CLOSE,
+         NULL
+      );
+
+      gtk_message_dialog_set_markup(GTK_MESSAGE_DIALOG(dialog),
+         body.c_str());
+      gtk_dialog_run(GTK_DIALOG(dialog));
+      gtk_widget_destroy(dialog);
+    }
+
+    static void
     on_save_button(GtkWidget *widget, EditAccountWindow *self) {
       const gchar* domain =
         gtk_entry_get_text(self->window_objects_.domain_entry);
@@ -52,7 +95,7 @@ EditAccountWindow::Init(CefRefPtr<Account> account, bool switch_on_save) {
   window_objects_.login_entry = GTK_ENTRY(gtk_builder_get_object(builder, "login_entry"));
   window_objects_.password_entry = GTK_ENTRY(gtk_builder_get_object(builder, "password_entry"));
 
-
+  g_signal_connect(gtk_builder_get_object(builder, "check_button"), "clicked", G_CALLBACK(on_check_button), this);
   g_signal_connect(gtk_builder_get_object(builder, "save_button"), "clicked", G_CALLBACK(on_save_button), this);
   g_signal_connect(gtk_builder_get_object(builder, "cancel_button"), "clicked", G_CALLBACK(on_cancel_button), this);
 
