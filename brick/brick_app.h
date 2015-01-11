a70347478c77f09d9a9538bd0bb4ddd94e892b36
Buglloc <buglloc@yandex.ru>
2015-01-11 21:34:08 +0200

    Version synchronized with PPA (0.1.4.29)

--- brick/brick_app.h
+++ brick/brick_app.h
@@ -2,7 +2,7 @@
 #define BRICK_APP_H_
 #pragma once
 
-#define VERSION "0.1.1.29" // IM API version in last part!
+#define VERSION "0.1.4.29" // IM API version in last part!
 #define APP_VERSION "Robin-Brick/" VERSION
 #define CEF_PRODUCT_VERSION APP_VERSION " (BitrixDesktop/3.x compatible) Chrome/39.0.2171.0"
 #define APP_NAME "Brick"
