7788fdd78f453dd5a33b5827e22614be40bb037c
Buglloc <buglloc@yandex.ru>
2015-01-18 20:06:13 +0200

    Minor improvements

--- brick/cef_handler_linux.cpp
+++ brick/cef_handler_linux.cpp
@@ -2,7 +2,6 @@
 // reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
 
-#include <caca_conio.h>
 #include "cef_handler.h"
 
 #include "include/wrapper/cef_helpers.h"
