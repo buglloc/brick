17b36422ec7d2d13860ea2fcfae3d47305e06a83
Buglloc <buglloc@yandex.ru>
2015-02-09 19:09:03 +0200

    Version up to 0.1.11.29

--- brick/brick_app.h
+++ brick/brick_app.h
@@ -4,7 +4,7 @@
 
 // ToDo: move defines to separate file?
 
-#define VERSION "0.1.10.29" // IM API version in last part!
+#define VERSION "0.1.11.29" // IM API version in last part!
 #define APP_VERSION "Robin-Brick/" VERSION
 #define CEF_PRODUCT_VERSION APP_VERSION " (BitrixDesktop/3.x compatible) Chrome/39.0.2171.0"
 #define APP_NAME "Brick"
