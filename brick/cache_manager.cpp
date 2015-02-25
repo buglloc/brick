c42c7fab04602bf4754333c73f99b310a017f0cb
Buglloc <buglloc@yandex.ru>
2015-02-25 23:26:13 +0200

    CacheManager

---
+++ brick/cache_manager.cpp
@@ -0,0 +1,80 @@
+#include <include/cef_url.h>
+#include "cache_manager.h"
+#include "brick_app.h"
+#include "helper.h"
+#include <ftw.h>
+#include <unistd.h>
+
+namespace {
+    time_t old_file_time;
+
+    const std::string cache_dir_[] = {
+       "other",
+       "buddy"
+    };
+
+    int
+    RemoveOldFile(const char *path, const struct stat *sb, int type) {
+      if (type == FTW_F) {
+        struct stat stat_data;
+        if (stat(path, &stat_data) != -1) {
+          if (stat_data.st_atim.tv_sec < old_file_time)
+            unlink(path);
+        }
+      }
+
+      // tell ftw to continue
+      return 0;
+    }
+}
+
+CacheManager::CacheManager()
+   : cache_path_("") {
+
+};
+
+void
+CacheManager::Init(const std::string& path) {
+  cache_path_ = path;
+}
+
+const std::string
+CacheManager::GetCacheDir(TYPE type) {
+  if (cache_path_.empty()) {
+    cache_path_ = std::string(BrickApp::GetCacheHome()) + "/" + APP_COMMON_NAME + "/app/";
+  }
+
+  return cache_path_ + cache_dir_[type];
+}
+
+const std::string
+CacheManager::GetCachePath(const std::string& url, TYPE type) {
+  std::string result = GetCacheDir(type);
+  std::string hash = std::to_string(helper::HashString(url));
+  CefURLParts parts;
+  CefParseURL(url, parts);
+
+  if (parts.host.length) {
+    std::string host = CefString(&parts.host);
+    result += "/" + host;
+  }
+
+  result  += "/" + hash.substr(0, 1) + "/" + hash;
+
+  if (parts.path.length) {
+    std::string file_path = CefString(&parts.path);
+    std::string ext = helper::GetFileExtension(file_path);
+    if (!ext.empty()) {
+      result += "." + ext;
+    }
+  }
+
+  return result;
+}
+
+void CacheManager::CleanUpCache() {
+  struct timespec spec;
+  clock_gettime(CLOCK_REALTIME, &spec);
+  old_file_time = spec.tv_sec - 86400 * 31;
+  ftw(cache_path_.c_str(), RemoveOldFile, FTW_F);
+}
---
+++ brick/cache_manager.h
@@ -0,0 +1,31 @@
+#ifndef BRICK_CACHE_MANAGER_H_
+#define BRICK_CACHE_MANAGER_H_
+#pragma once
+
+#include <string>
+#include <include/cef_base.h>
+
+class CacheManager : public CefBase {
+
+public:
+
+  enum TYPE {
+    DEFAULT = 0,
+    BUDDY_ICON
+  };
+
+  CacheManager();
+  ~CacheManager() {};
+
+  void Init(const std::string& path);
+  const std::string GetCacheDir(TYPE type = TYPE::DEFAULT);
+  const std::string GetCachePath(const std::string& url, TYPE type = TYPE::DEFAULT);
+  void CleanUpCache();
+
+protected:
+  std::string cache_path_;
+
+IMPLEMENT_REFCOUNTING(CacheManager);
+};
+
+#endif // BRICK_CACHE_MANAGER_H_
