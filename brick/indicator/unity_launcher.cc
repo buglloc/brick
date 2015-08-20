25d757b88cf2b42f0a7a11452b0a3896acbd7fbc
Buglloc <buglloc@yandex.ru>
2015-08-21 00:20:30 +0200

    Простенькая реализация интеграции с UnityLauncher

---
+++ brick/indicator/unity_launcher.cc
@@ -0,0 +1,65 @@
+#include "brick/indicator/unity_launcher.h"
+#include "brick/brick_app.h"
+#include "include/base/cef_logging.h"
+
+void
+UnityLauncher::Init() {
+  handler_ = unity_launcher_entry_get_for_desktop_id(APP_COMMON_NAME ".desktop");
+  RegisterEventListeners();
+}
+
+void
+UnityLauncher::onEvent(const DownloadProgressEvent& event) {
+  if (event.getTotal() <= 0)
+    return;
+
+  if (event.getPercent() >= 100 && downloads_.count(event.getId())) {
+    downloads_.erase(event.getId());
+  }
+
+  downloads_[event.getId()] = event.getPercent();
+  Update();
+}
+
+void
+UnityLauncher::onEvent(const DownloadCompleteEvent& event) {
+  if (downloads_.count(event.getId())) {
+    downloads_.erase(event.getId());
+  }
+  Update();
+}
+
+void
+UnityLauncher::Update() {
+  int badges = badges_ + static_cast<int>(downloads_.size());
+  if (badges > 0) {
+    unity_launcher_entry_set_count(handler_, badges);
+    unity_launcher_entry_set_count_visible(handler_, true);
+    if (!downloads_.empty()) {
+      unity_launcher_entry_set_progress_visible(handler_, true);
+      double progress = 0;
+      for (auto download: downloads_) {
+        progress += download.second;
+      }
+      LOG(WARNING) << "prog: " << progress / downloads_.size() / 100;
+      unity_launcher_entry_set_progress(handler_, progress / downloads_.size() / 100);
+    }
+  } else {
+    unity_launcher_entry_set_count(handler_, 0);
+    unity_launcher_entry_set_count_visible(handler_, false);
+    unity_launcher_entry_set_progress(handler_, 0);
+    unity_launcher_entry_set_progress_visible(handler_, false);
+  }
+}
+
+void
+UnityLauncher::SetBadge(int badge) {
+  badges_ = badge;
+  Update();
+}
+
+void
+UnityLauncher::RegisterEventListeners() {
+  EventBus::AddHandler<DownloadProgressEvent>(*this);
+  EventBus::AddHandler<DownloadCompleteEvent>(*this);
+}
---
+++ brick/indicator/unity_launcher.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 The Brick Authors.
+
+#ifndef BRICK_INDICATOR_UNITY_LAUNCHER_H_
+#define BRICK_INDICATOR_UNITY_LAUNCHER_H_
+#pragma once
+
+#include <unity.h>
+
+#include <map>
+
+#include "include/cef_base.h"
+#include "brick/event/event_bus.h"
+#include "brick/event/download_progress_event.h"
+#include "brick/event/download_complete_event.h"
+
+class UnityLauncher : public CefBase,
+                      public EventHandler<DownloadProgressEvent>,
+                      public EventHandler<DownloadCompleteEvent>{
+
+public:
+  void Init();
+  void SetBadge(int badge);
+  // System events handler
+  virtual void onEvent(const DownloadProgressEvent& event) OVERRIDE;
+  virtual void onEvent(const DownloadCompleteEvent& event) OVERRIDE;
+
+protected:
+  void RegisterEventListeners();
+  void Update();
+
+private:
+  int badges_;
+  std::map<std::string, int> downloads_;
+  UnityLauncherEntry *handler_;
+
+IMPLEMENT_REFCOUNTING(UnityLauncher);
+};
+
+#endif  // BRICK_INDICATOR_UNITY_LAUNCHER_H_
